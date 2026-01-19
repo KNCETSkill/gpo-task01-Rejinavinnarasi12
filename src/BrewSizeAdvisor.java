0d5add475816499c2608fb74afd5d9905836072e
Rejina Vinnarasi S <rejinavinnarasi120518@gmail.com>
2026-01-19 19:13:41 +0845

    Update BrewSizeAdvisor.java

--- src/BrewSizeAdvisor.java
+++ src/BrewSizeAdvisor.java
@@ -10,7 +10,7 @@ public class BrewSizeAdvisor {
 if(volume<200){
     System.out.println("Small");
 }
- else if(volume>=200 && volume<=350){
+  if else(volume>=200 && volume<=350){
      System.out.println("Medium");
          }
         else(volume>350)
